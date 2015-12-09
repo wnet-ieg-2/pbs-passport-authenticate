19a558197a85c12f9a436f76965de9ce34302a79
Brian Santalone <santalone@wnet.org>
2015-12-09 16:12:50 -0500

    no message

--- assets/js/jquery.pids.js
+++ assets/js/jquery.pids.js
@@ -146,14 +146,16 @@ jQuery(document).ready(function($) {
 			if (user) {
 				// if user already logged in
 				var memberid = getQueryStringParamPBS('membership_id');
-				var destination = Cookies.getJSON('pbsoauth_login_referrer');
+				
 				$.ajax({
 			      url: activatelink,
 			      data: 'membership_id=' + memberid + '',
 			      type: 'POST',
 			      dataType: 'json',
 			      success: function(response) {
-			        window.location.href = destination;
+				  	var destination = Cookies.getJSON('pbsoauth_login_referrer');
+			        if (window.location.href != destination) {window.location.href = destination; console.log('test1');}
+					else {window.location.href = "/"; console.log('test2');}
 			      }
 			    });
 			}
