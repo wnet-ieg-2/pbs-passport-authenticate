e40b8516c71f93f59dd67f57be6007c25828528a
William Tam <tamw@wnet.org>
2015-12-14 11:12:40 -0500

    changing secrets to password input

--- classes/class-pbs-passport-authenticate-settings.php
+++ classes/class-pbs-passport-authenticate-settings.php
@@ -65,13 +65,13 @@ class PBS_Passport_Authenticate_Settings {
 
     add_settings_field( 'oauth2_endpoint', 'oAuth2 Endpoint', array( $this, 'settings_field'), 'pbs_passport_authenticate', 'pbslaas_settings', array('setting' => 'pbs_passport_authenticate', 'field' => 'oauth2_endpoint', 'class' => 'regular-text', 'label' => 'Root path for PBS-provided oAuth endpoints to PBS etc. This should only change if authenticating against a dev endpoint.', 'default' => 'https://account.pbs.org/oauth2/' ) );
     add_settings_field( 'laas_client_id', 'LAAS Client ID', array( $this, 'settings_field'), 'pbs_passport_authenticate', 'pbslaas_settings', array('setting' => 'pbs_passport_authenticate', 'field' => 'laas_client_id', 'class' => 'regular-text', 'label' => 'Client ID for PIDS/LAAS.  Provided by PBS.') );
-    add_settings_field( 'laas_client_secret', 'LAAS Client secret', array( $this, 'settings_field'), 'pbs_passport_authenticate', 'pbslaas_settings', array('setting' => 'pbs_passport_authenticate', 'field' => 'laas_client_secret', 'class' => 'large-text', 'label' => 'Client Secret for PIDS/LAAS.  Provided by PBS.') );
+    add_settings_field( 'laas_client_secret', 'LAAS Client secret', array( $this, 'settings_field'), 'pbs_passport_authenticate', 'pbslaas_settings', array('setting' => 'pbs_passport_authenticate', 'field' => 'laas_client_secret', 'type' => 'password', 'class' => 'large-text', 'label' => 'Client Secret for PIDS/LAAS.  Provided by PBS.') );
 
     add_settings_section('mvault_settings', 'Membership Vault settings', array( $this, 'settings_section_callback'), 'pbs_passport_authenticate');
 
     add_settings_field( 'mvault_endpoint', 'MVault API Endpoint', array( $this, 'settings_field'), 'pbs_passport_authenticate', 'mvault_settings', array('setting' => 'pbs_passport_authenticate', 'field' => 'mvault_endpoint', 'class' => 'regular-text', 'label' => 'Membership Vault API URL. This should only change if authenticating against a dev endpoint.', 'default' => 'https://mvault.services.pbs.org/api/' ) );
     add_settings_field( 'mvault_client_id', 'MVault API Client ID', array( $this, 'settings_field'), 'pbs_passport_authenticate', 'mvault_settings', array('setting' => 'pbs_passport_authenticate', 'field' => 'mvault_client_id', 'class' => 'regular-text', 'label' => 'MVault API Client ID. Provided by PBS.') );
-    add_settings_field( 'mvault_client_secret', 'MVault API Client Secret', array( $this, 'settings_field'), 'pbs_passport_authenticate', 'mvault_settings', array('setting' => 'pbs_passport_authenticate', 'field' => 'mvault_client_secret', 'class' => 'regular-text', 'label' => 'MVault API Client Secret. Provided by PBS.') );
+    add_settings_field( 'mvault_client_secret', 'MVault API Client Secret', array( $this, 'settings_field'), 'pbs_passport_authenticate', 'mvault_settings', array('setting' => 'pbs_passport_authenticate', 'field' => 'mvault_client_secret', 'type' => 'password', 'class' => 'regular-text', 'label' => 'MVault API Client Secret. Provided by PBS.') );
 	}
 
 	public function settings_section_callback() { echo ' '; }
@@ -83,9 +83,10 @@ class PBS_Passport_Authenticate_Settings {
     $field = esc_attr( $args['field'] );
     $label = esc_attr( $args['label'] );
     $class = esc_attr( $args['class'] );
+    $type = ($args['type'] ? esc_attr( $args['type'] ) : 'text' );
     $default = ($args['default'] ? esc_attr( $args['default'] ) : '' );
     $value = (($setting[$field] && strlen(trim($setting[$field]))) ? $setting[$field] : $default);
-    echo '<input type="text" name="' . $settingname . '[' . $field . ']" id="' . $settingname . '[' . $field . ']" class="' . $class . '" value="' . $value . '" /><p class="description">' . $label . '</p>';
+    echo '<input type="' . $type . '" name="' . $settingname . '[' . $field . ']" id="' . $settingname . '[' . $field . ']" class="' . $class . '" value="' . $value . '" /><p class="description">' . $label . '</p>';
 	}
 
 
