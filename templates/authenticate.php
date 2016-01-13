fb97685a98d012a009e2eeb3a7124647483eedc6
William Tam <tamw@wnet.org>
2016-01-13 14:54:46 -0500

    Revert "actually removing the commented out code"
    This reverts commit fb9aef2fea9771ac6d86e3f475a6a530660bdaa6.
    
    moving to its own branch

--- templates/authenticate.php
+++ templates/authenticate.php
@@ -40,6 +40,12 @@ $laas_client = $passport->get_laas_client();
 $rememberme = (isset($_POST["rememberme"])) ? $_POST["rememberme"] : false;
 $nonce = (isset($_POST["nonce"])) ? $_POST["nonce"] : false;
 $errors = array();
+/* pointless and to be removed, since its in the oauthcallback
+if (isset($_POST["code"])){
+  $code = $_POST["code"];
+  $userinfo = $laas_client->authenticate($code, $rememberme, $nonce);
+} else 
+*/
 if (isset($_REQUEST["logout"])) {
   $userinfo = $laas_client->logout();
 } else {
