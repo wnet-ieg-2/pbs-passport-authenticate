047d0724a4a2d3c46ae3ec0f03be2017effbb6ba
William Tam <tamw@wnet.org>
2016-10-06 18:59:10 +0000

    making the create pbs account link functional

--- templates/loginform.php
+++ templates/loginform.php
@@ -76,7 +76,7 @@ echo "</div>";
 	<li class="facebook"><a href="<?php echo($links['facebook']); ?>" title="Sign in with Facebook"><img src="<?php echo $pluginImageDir; ?>/sign-in-facebook.png" /></a></li>
 	<li class='or'><span>OR</span></li>
 	<li class="pbs"><a href="<?php echo($links['pbs']); ?>" title="Sign in with PBS Account"><img src="<?php echo $pluginImageDir; ?>/sign-in-pbs.png" /></a>
-		<div class='create-pbs'>Don't have a PBS account? <a href="<?php echo($links['pbs']); ?>">Create one!</a></div>
+		<div class='create-pbs'>Don't have a PBS account? <a href="<?php echo($links['create_pbs']); ?>">Create one!</a></div>
 	</li>
 	<li class="stay-logged-in"><input type="checkbox" id="pbsoauth_rememberme" name="pbsoauth_rememberme" value="true" checked /> Keep me logged in on this device</li>
 	</ul>
