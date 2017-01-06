7b505583edeb7488a40a1704123e7ce96689e935
William Tam <tamw@wnet.org>
2017-01-06 20:50:26 +0000

    making cove-passport shortcode functional without wnet video utilities plugin

--- build-passport-player.php
+++ build-passport-player.php
@@ -1,18 +1,27 @@
 <?php 
 if (!function_exists('build_passport_player')) {
 function build_passport_player($video) {
-
+  if (empty($video->tp_media_object_id)) {
+    return;
+  }
 	global $coveWindow;
 	$imgDir = get_bloginfo('template_directory');
 	$m = json_decode($video->metadata);
 	
 	// video poster image. 
-	if (empty($m->mezzanine)) {$large_thumb = $imgDir . "/libs/images/default.png";}
-	else {
-		if (function_exists( 'wnet_video_cove_thumb')) {$large_thumb = wnet_video_cove_thumb($m->mezzanine, 1200, 675);}
-		else {$large_thumb = $m->mezzanine;}
+	if (empty($m->mezzanine)) {
+    $large_thumb = $imgDir . "/libs/images/default.png";
+  } else {
+		if (function_exists( 'wnet_video_cove_thumb')) {
+      $large_thumb = wnet_video_cove_thumb($m->mezzanine, 1200, 675);
+    } else {
+      $large_thumb = $m->mezzanine;
+    }
 	}
-	
+  if ( ($video->window != 'all_members') && ( $video->window != 'station_members') ) {
+    // this video is public, just show it
+    return '<div class="passportcoveplayer" data-window="public" data-media="' . $video->tp_media_object_id . '"><div class="embed-container video-wrap"><iframe id="partnerPlayer" marginwidth="0" marginheight="0" scrolling="no" src="//player.pbs.org/widget/partnerplayer/' . $video->tp_media_object_id . '/?chapterbar=false" allowfullscreen="" frameborder="0"></iframe></div></div>';
+  }
 	// passport video overlay for gated videos.
 	if (PASSPORT_ENABLED && (($video->window == 'all_members') ||( $video->window == 'station_members')) && $coveWindow == 'all') {
 		
@@ -34,40 +43,48 @@ function build_passport_player($video) {
 				</div>
 			</div></div>";
 			$passportGated = "gated";
-		}
-		else {$passportOverlay = ""; $passportGated = "standard";}
+		} else {
+      $passportOverlay = ""; 
+      $passportGated = "standard";
+    }
 
 		// if passport not enabled, and video expired.
 		if (!PASSPORT_ENABLED && (($video->window == 'all_members') ||( $video->window == 'station_members'))) {
 			$passportError = "<div class='passport-error'><span>This video is currently unavailable on $StationTheme</span></div>";
-		}
-		else {$passportError = "";}
-		
-        return '<div class="passportcoveplayer" data-window="'.$video->window.'" data-media="'.$video->tp_media_object_id.'"><div class="passport-'.$passportGated.'-video"><img src="'.$large_thumb.'">'.$passportOverlay . $passportError.'</div></div>';
+		} else {
+      $passportError = "";
     }
+    return '<div class="passportcoveplayer" data-window="'.$video->window.'" data-media="'.$video->tp_media_object_id.'"><div class="passport-'.$passportGated.'-video"><img src="'.$large_thumb.'" >'.$passportOverlay . $passportError.'</div></div>';
+   }
 }
 
 function cove_passport_shortcode($atts, $content = null) {
   
-  extract(shortcode_atts(array("id" => ''), $atts));
+  extract(shortcode_atts(array(
+                            "id" => '',
+                            "window" => '',
+                            "image" => ''
+                        ), $atts));
 
   $player = "<!-- video no longer available -->";
-  
-  $video = pbs_video_utils_get_video($id);
- 
+  if (function_exists('pbs_video_utils_get_video')) {
+    $video = pbs_video_utils_get_video($id);
+  } else {
+    $video = array(
+        array ( "tp_media_object_id" => $id,
+          "window" => $window,
+          "metadata" => json_encode(array ( "mezzanine" => $image ))
+        )
+    );
+  }
   if (!empty($video)) {
     $video = json_decode(json_encode($video));
     $player = "<div class='shortcode-video cf'>". build_passport_player($video[0]) . "</div>";
-    
   }
-  elseif (!empty($video)) {
-    $video = json_decode(json_encode($video));
-    $player = "<div class='shortcode-video cf'>". build_cove_player($video[0]) . "</div>";
-  }
-  
   return $player;
 
 }
-add_shortcode("cove-passport", "cove_passport_shortcode");  
-
+if (!shortcode_exists("cove-passport")) {
+  add_shortcode("cove-passport", "cove_passport_shortcode");  
+}
 /* end of file */
