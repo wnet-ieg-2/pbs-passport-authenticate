626eade8b073f71eb4500128efd61b5149de3333
William Tam <tamw@wnet.org>
2019-11-08 22:35:13 +0000

    updated layout for userinfo

--- templates/userinfo.php
+++ templates/userinfo.php
@@ -41,8 +41,6 @@ echo "<div class='ppa-wrap cf'>";
 echo "<div class='ppa-primary userinfo-block'>";
 	
 	
-//echo "<h3>USER STATUS</h3>";
-echo "<div class='passport-username'><strong>MEMBER:</strong> " . $userinfo['first_name'] . " " . $userinfo['last_name'] . "</div>";
 
 //echo print_r($mvaultinfo);
   $station_nice_name = $defaults['station_nice_name'];
@@ -52,29 +50,31 @@ echo "<div class='passport-username'><strong>MEMBER:</strong> " . $userinfo['fir
 
 /* active member */
 if ( !empty($userinfo['membership_info']['offer']) && $userinfo['membership_info']['status'] == "On" && $userinfo['vppa_status'] == 'valid') {
+  echo "<div class='passport-username'><strong>MEMBER:</strong> " . $userinfo['first_name'] . " " . $userinfo['last_name'] . "</div>";
 	echo "<p class='passport-status'>$station_nice_name Passport <i class='fa fa-check-circle passport-green'></i></p>";
-	if (!empty($watch_url)) {echo "<p><a href='$watch_url'><button class='pp-button-blue'>Watch Programs <i class='fa fa-arrow-circle-right'></i></button></a></p>";}
+	if (!empty($watch_url)) {echo "<div class='activate-options cf'><ul><li class='service-login-link watch'><p><a href='$watch_url'><button class='pp-button-blue'>Watch Programs</button></a></p></li></ul></div>";}
 }
 
 /* not an active member */
 elseif ( empty($userinfo['membership_info']['offer']) && $userinfo['membership_info']['status'] == "Off") {
+  echo "<div class='passport-username'><strong>NAME:</strong> " . $userinfo['first_name'] . " " . $userinfo['last_name'] . "</div>";
 	$active_url = site_url('pbsoauth/activate');
 	echo "<div class='login-wrap cf'><ul>";
-	echo "<li><p class='passport-status'>$station_nice_name Passport <span class='passport-exclamation'><i class='fa fa-exclamation'></i></span></p></li>";
+	echo "<li><p class='passport-status'><strong>STATUS:</strong> Not activated <span class='passport-exclamation'><i class='fa fa-exclamation'></i></span></p></li>";
 	
 	
 	
-	echo "<li class='passport-not-setup'><p>Your $station_nice_name Passport account is not setup.
+	echo "<li class='passport-not-setup'><p>Your $station_nice_name Passport account is not setup with the account you've logged in with.
 $station_nice_name Passport is a benefit for eligible members of $station_nice_name.</p>
 
-	<p>If you are a member. please choose an option below. If you are not a member, use the \"Become a Member\" button.</p> </li>";
+	<p>If you are a member, please choose an option below. If you are not a member, use the \"Become a Member\" button.</p> </li>";
 	
 	
 	echo "</ul></div>";
 	
 	
 	echo "<div class='activate-options cf'><ul>";
-	echo "<li class='service-login-link activate'><h4>I'm a member <strong>with</strong> an activation code</h4><a href='$active_url'><button class='pp-button-outline'>Activate Account <span class='icon-passport'></span></button></a></li>";
+	echo "<li class='service-login-link activate'><p>I'm a member <strong>with</strong> an activation code</p><a href='$active_url'><button class='pp-button-blue'>Activate Account <span class='icon-passport'></span></button></a></li>";
 
   $memberlookuplink = 'https://www.pbs.org/passport/lookup/';
   $memberlookuptext = 'Request Activation Code';
@@ -83,9 +83,12 @@ $station_nice_name Passport is a benefit for eligible members of $station_nice_n
     $memberlookuplink = site_url('pbsoauth/alreadymember');
     $memberlookuptext = 'Request Account Setup';
   } 
-	echo "<li class='service-login-link accountsetep'><h4>I'm a member <strong>without</strong> an activation code</h4><a href='" . $memberlookuplink . "'><button class='pp-button-outline'>$memberlookuptext</button></a></li>";
-	if (!empty($join_url)) { echo "<li class='single'><h4>Not a Member?</h4><a href='$join_url'><button class='pp-button-outline'>Become a Member <i class='fa fa-heart-o'></i></button></a></li>";}
-	echo "</ul></div><!-- .activate-options -->";
+	echo "<li class='service-login-link accountsetep'><p>I'm a member <strong>without</strong> an activation code</p><a href='" . $memberlookuplink . "'><button class='pp-button-blue'>$memberlookuptext</button></a></li>";
+	if (!empty($join_url)) { echo "<li class='service-login-link join'><p>Not a Member?</p><a href='$join_url'><button class='pp-button-blue'>Become a Member</button></a></li>";}
+
+  //echo "<li class='service-login-link loginproblem'><p>I have <strong>already activated</strong></p><div class='pbs_passport_authenticate'><a class='signout'><button class='pp-button-blue'>Try a different login</button></a></div></li>";
+
+	echo "</ul></div><!-- .activate-options --><p>If you have an activated $station_nice_name Passport membership that you have previously used, you may currently be signed in with a different login than what you used to activated your account. If you think this is the case, <span class='pbs_passport_authenticate'><a class='signout'>sign out</a></span> and try 'Member Sign In' again with a different account.</p>";
 
 }
 
@@ -97,8 +100,9 @@ elseif ( $userinfo['vppa_status'] != 'valid') {
 
 /* expired member */
 else {
-	echo "<p class='passport-status'>" . $defaults['station_nice_name'] . " Passport <i class='fa fa-times-circle passport-red'></i></p>";
-	if (!empty($join_url)) {echo "<p><a href='$join_url' class='passport-button'>Renew Membership</a></p>";}
+  echo "<div class='passport-username'><strong>MEMBER:</strong> " . $userinfo['first_name'] . " " . $userinfo['last_name'] . "</div>";
+	echo "<p class='passport-status'><strong>STATUS:</strong> Expired <i class='fa fa-times-circle passport-red'></i></p>";
+	if (!empty($join_url)) {echo "<p>Your $station_nice_name Passport membership has expired.  Please renew your $station_nice_name membership to continue enjoying $station_nice_name Passport content.</p><div class='activate-options cf'><ul><li class='service-login-link watch'><a href='$join_url'><button class='pp-button-blue'>Renew Membership</button></a></li></ul></div>";}
 }
 
 
@@ -114,14 +118,12 @@ else {
 		
 		<div class="ppa-box help">
 			<h3 class="boxhead">NEED HELP?</h3>
-			<p>Have more questions or technical problems?</p>
-			<p><a href="<?php echo $defaults['help_url']; ?>"><i class='fa fa-info-circle'></i> VISIT OUR FAQS</a></p>
-			<p><a href="tel:<?php echo $defaults['help_phone']; ?>"><i class="fa fa-phone-square"></i> CONTACT <?php echo $defaults['help_phone']; ?></a></p>
+      <?php echo $defaults['help_text']; ?>
 		</div>
 		
 		<div class="ppa-box">
 			<h3 class="boxhead">About <?php echo $defaults['station_nice_name']; ?> Passport</h3>
-			<?php echo wpautop('@will call a text field from the admin here') ?>
+      <p><?php echo $defaults['station_nice_name']; ?> Passport is the member benefit that provides you with extended access to an on-demand library of quality public television programming, including current and past seasons of PBS shows.</p>
 		</div>
 		
 	</div>
