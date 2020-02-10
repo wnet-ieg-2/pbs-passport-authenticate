decec715375a27b750d41d6d31ab2bfa51a0b5f8
Brian Santalone <santalone@wnet.org>
2020-02-10 16:05:24 -0500

    no message

--- templates/userinfo.php
+++ templates/userinfo.php
@@ -38,9 +38,7 @@ echo "<div class='ppa-wrap cf'>";
 echo "<div class='ppa-primary userinfo-block'>";
 	
 	
-	if (!empty($defaults['station_passport_logo'])) {
-  echo '<div class="pp-logo-head"><img src="' . $defaults['station_passport_logo'] . '" /></div>'; 
-}
+	
 
 	
 
@@ -49,17 +47,31 @@ echo "<div class='ppa-primary userinfo-block'>";
   $join_url = $defaults['join_url'];
   $watch_url = $defaults['watch_url'];
   
+	
+	
+	echo "<div class='passport-username'><strong>MEMBER:</strong> " . $userinfo['first_name'] . " " . $userinfo['last_name'] . "</div>";
+	
+	
 
 /* active member */
 if ( !empty($userinfo['membership_info']['offer']) && $userinfo['membership_info']['status'] == "On" && $userinfo['vppa_status'] == 'valid') {
-  echo "<div class='passport-username'><strong>MEMBER:</strong> " . $userinfo['first_name'] . " " . $userinfo['last_name'] . "</div>";
+  
+	if (!empty($defaults['station_passport_logo'])) {
+		echo '<div class="pp-logo-head"><img src="' . $defaults['station_passport_logo'] . '" /></div>'; 
+	}
+	
 	echo "<p class='passport-status'>$station_nice_name Passport <i class='fa fa-check-circle passport-green'></i></p>";
 	if (!empty($watch_url)) {echo "<div class='activate-options cf'><ul><li class='service-login-link watch'><p><a href='$watch_url'><button class='pp-button-blue'>Watch Programs</button></a></p></li></ul></div>";}
 }
 
 /* not an active member */
 elseif ( empty($userinfo['membership_info']['offer']) && $userinfo['membership_info']['status'] == "Off") {
-  echo "<div class='passport-username'><strong>NAME:</strong> " . $userinfo['first_name'] . " " . $userinfo['last_name'] . "</div>";
+ 
+	if (!empty($defaults['station_passport_logo'])) {
+		echo '<div class="pp-logo-head"><img src="' . $defaults['station_passport_logo'] . '" /></div>'; 
+	}
+	
+	
 	$active_url = site_url('pbsoauth/activate');
 	echo "<div class='login-wrap cf'><ul>";
 	echo "<li><p class='passport-status'><strong>STATUS:</strong> Not activated <span class='passport-exclamation'><i class='fa fa-exclamation'></i></span></p></li>";
