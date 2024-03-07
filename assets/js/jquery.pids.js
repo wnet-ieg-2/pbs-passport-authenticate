6535425fbd13c79557303c0b8fcc2aea044f71f7
William Tam <tamw@wnet.org>
2024-03-07 13:57:49 -0500

    further tweak to dropdown

--- assets/js/jquery.pids.js
+++ assets/js/jquery.pids.js
@@ -202,10 +202,10 @@ jQuery(document).ready(function($) {
         if ($(".pbs_passport_authenticate div.messages-new")[0]){
             // new version of messages with drop down menu...
             if (user.thumbnail_URL) {thumbimage = "<span><img src='" + user.thumbnail_URL + "' alt='' /></span>";}
-            portal_list_entry = '<li><a href="' + userinfolink + '">YOUR ACCOUNT</a></li>';
+            portal_list_entry = '<li><a href="' + userinfolink + '">USER STATUS</a></li>';
             if (portallink.length > 0) {
               if (userPBSLoginStatus.memberStatus == 'valid' || userPBSLoginStatus.memberStatus == 'expired') {
-                portal_list_entry= '<li><a href="' + portallink + '" target=_new >MEMBER PORTAL</a></li>';
+                portal_list_entry .= '<li><a href="' + portallink + '" target=_new >MEMBER PORTAL</a></li>';
               }
             }
              welcomestring = '<a href="#" class="toggle-user-drop userlink">' + thumbimage + '<span class="name">' + user.first_name + '</span></a> <ul class="user-drop">'+ portal_list_entry + '<li><a  class="signout">SIGN OUT</a></li></ul>';
