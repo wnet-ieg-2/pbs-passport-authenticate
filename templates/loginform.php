4716150bab22f0d9f92f55c410f74a02773bf5dd
Brian Santalone <santalone@wnet.org>
2024-09-23 13:54:46 -0400

    no message

--- templates/loginform.php
+++ templates/loginform.php
@@ -51,11 +51,16 @@ if ($use_pmsso) {
 	<html><head>
 	<script type="text/javascript" src="<?php echo get_site_url(); ?>/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
 	<script type="text/javascript" src="<?php echo($passport->assets_url); ?>js/pkce_loginform.js"></script>
+	<style>body {background: #000525; color: #fff; font: bold 1.125em sans-serif; margin: 0; padding: 0;} a {color: #fff;} svg {vertical-align: middle;} a {text-decoration: none; color: #2b92ff;} a:hover {text-decoration: underline; color: #2b92ff;} .svg-txt {vertical-align: middle;} .svg-spin { -webkit-animation: svg-spin 2s infinite linear; animation: svg-spin 2s infinite linear; } .passport-login-wrap {height: 100vh; display: flex; flex-wrap: wrap; flex-flow: row wrap; align-items: center; justify-content: center;} @keyframes svg-spin { 0% { -webkit-transform: rotate(0deg); transform: rotate(0deg);} 100% { -webkit-transform: rotate(359deg); transform: rotate(359deg); }}</style>
 	</head>
 	<body>
-	<div class='passport-login-wrap'><li class="pmsso"><a href='<?php echo $pmsso_url; ?>'>Please Wait...</a></li></div>
+	<div class='passport-login-wrap'><div class='login-inner'>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 1em; height: 1em;" class="svg-spin"><path fill="#fff" d="M224 32c0-17.7 14.3-32 32-32C397.4 0 512 114.6 512 256c0 46.6-12.5 90.4-34.3 128c-8.8 15.3-28.4 20.5-43.7 11.7s-20.5-28.4-11.7-43.7c16.3-28.2 25.7-61 25.7-96c0-106-86-192-192-192c-17.7 0-32-14.3-32-32z"/><path fill="#2b92ff" d="M256 64C150 64 64 150 64 256s86 192 192 192c70.1 0 131.3-37.5 164.9-93.6l.1 .1c-6.9 14.9-1.5 32.8 13 41.2c15.3 8.9 34.9 3.6 43.7-11.7c.2-.3 .4-.6 .5-.9l0 0C434.1 460.1 351.1 512 256 512C114.6 512 0 397.4 0 256S114.6 0 256 0c-17.7 0-32 14.3-32 32s14.3 32 32 32z"/></svg>
+		<span class='svg-txt'><a href='<?php echo $pmsso_url; ?>' class="pmsso">Please Wait...</a></span>
+	</div></div>
 	</body></html>
 	<?php
+
 } else {
 
 get_header();
